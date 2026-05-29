c221940cbcb3a62a706615aa8a0be477a617ba95
Amjad <amjadnewt@gmail.com>
2026-05-29 02:33:32 +0200

    fix mail timeoutn

--- config.py
+++ config.py
@@ -17,9 +17,12 @@ class Config:
     MAIL_SERVER = "smtp.gmail.com"
     MAIL_PORT = 587
     MAIL_USE_TLS = True
+    MAIL_USE_SSL = False
+    MAIL_TIMEOUT = 10
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     MAIL_DEFAULT_SENDER = os.environ.get("MAIL_USERNAME")
 
     STRIPE_SECRET_KEY = os.environ.get("STRIPE_SECRET_KEY")
     STRIPE_PUBLIC_KEY = os.environ.get("STRIPE_PUBLIC_KEY")
+    
